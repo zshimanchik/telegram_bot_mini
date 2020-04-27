b741eb1fc52c8b623d96d685c9607cab9fce7be0
Zahar Shimanchik <zahar.shimanchik@yandex.ru>
2020-04-27 14:26:06 +0200

    Implemented BotWasBlockedException

--- telegram_bot_mini/bot_api.py
+++ telegram_bot_mini/bot_api.py
@@ -1,5 +1,12 @@
 import requests
 
+class TelegramApiException(Exception):
+    pass
+
+
+class BotWasBlockedException(TelegramApiException):
+    pass
+
 
 class TelegramBotApi:
     def __init__(self, token):
@@ -11,13 +18,17 @@ class TelegramBotApi:
         if keyboard is not None:
             data['reply_markup'] = {"keyboard": keyboard}
         url = self.base_url + "/sendMessage"
-        response = requests.post(url, json=data)
-        response.raise_for_status()
+        self._send_request(url, data)
 
     def send_photo(self, chat_id, photo, keyboard=None):
         data = {"photo": photo, "chat_id": chat_id}
         if keyboard is not None:
             data['reply_markup'] = {"keyboard": keyboard}
         url = self.base_url + "/sendPhoto"
+        self._send_request(url, data)
+
+    def _send_request(self, url, data):
         response = requests.post(url, json=data)
+        if response.status_code == 403 and 'bot was blocked' in response.text:
+            raise BotWasBlockedException()
         response.raise_for_status()
